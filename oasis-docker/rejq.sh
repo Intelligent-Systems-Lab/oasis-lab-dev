904994d6939c505e9d10b035dcc9677be4656d63
tony <tony92151@gmail.com>
2020-10-09 18:16:27 +0800

    update docker

--- oasis-docker/rejq.sh
+++ oasis-docker/rejq.sh
@@ -1,3 +1,10 @@
-echo "JSON formatter : $1 "
-jq . $1 > /tmp/tmp.json
-cp /tmp/tmp.json $1
+if [ -z "$1" ]
+then
+    echo "File not found."
+else
+    echo "JSON formatter : $1 "
+    jq . $1 > /tmp/tmp.json
+    cp /tmp/tmp.json $1
+fi
+
+
