983b3f3158a6b4233b22cc5034b48df343d4c673
tony <tony92151@gmail.com>
2020-10-10 21:48:31 +0800

    add testnet tools

--- testnet/genesis_preprocess/merge.py
+++ testnet/genesis_preprocess/merge.py
@@ -1,14 +1,107 @@
 import json
-import os
+import os, sys
 import argparse
+
+import subprocess
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--genesis_path', type=str, default=None)
 parser.add_argument('--example_path', type=str, default=None)
 
-parser.add_argument('-f')
-
 args = parser.parse_args()
 
 genesis_path = os.path.expanduser(args.genesis_path)
 example_path = os.path.expanduser(args.example_path)
+
+#print("Merge file :",genesis_path, " to ",example_path)
+
+if genesis_path==example_path or genesis_path==None or example_path==None:
+    sys.exit("File error.")
+
+################
+leger_example= '{"oasis1": {"escrow": {"active": {"balance": "200000000000","total_shares": "5000000000000000"},"commission_schedule": {"bounds": [{"rate_max": "20000","rate_min": "0"}],"rates": [{"rate": "5000"}]},"debonding": {"balance": "0","total_shares": "0"},"stake_accumulator": {}},"general": {"balance": "100000000000"}}}'
+
+delegations_exp = '{"oasis1": {"oasis2": {"shares": "4000000000000000"},"oasis3": {"shares": "1000000000000000"}}}'
+################
+
+
+# print("Merge file :",genesis_path, " to ",example_path)
+
+#print(json.dumps(leger_example, sort_keys=True, indent=4, separators=(',', ': ')))
+
+### Load teo file ###
+
+with open(genesis_path) as json_file:
+    json_gene = json.load(json_file)
+
+with open(example_path) as json_file:
+    json_exp = json.load(json_file)
+#####################
+
+json_gene_entity = json_gene['registry']['entities']
+json_gene_entity_len = len(json_gene_entity)
+
+json_gene_nodes = json_gene['registry']['nodes']
+json_gene_nodes_len = len(json_gene_entity)
+
+json_gene_total = 10000000000000000000
+
+# Add entities to exp file
+json_exp['registry']['entities'] = json_gene_entity
+# Add node to exp file
+json_exp['registry']['nodes'] = json_gene_nodes
+
+# Add delegations to exp file
+entities_account_list = []
+for i in json_gene_entity:
+    k = i['signature']['public_key']
+
+    proc = subprocess.Popen('oasis-node stake pubkey2address  --public_key '+k , shell=True,stdout=subprocess.PIPE)
+    outs, errs = proc.communicate(timeout=3)
+
+    entities_account_list.append(outs.decode('utf-8').splitlines()[0])
+
+nodes_account_list = []
+for i in json_gene_nodes:
+    k = i['signatures'][0]['public_key']
+
+    proc = subprocess.Popen('oasis-node stake pubkey2address  --public_key '+k , shell=True,stdout=subprocess.PIPE)
+    outs, errs = proc.communicate(timeout=3)
+
+    nodes_account_list.append(outs.decode('utf-8').splitlines()[0])
+
+# print(entities_account_list)
+# print(nodes_account_list)
+
+delegations_str = ''
+for i in range(len(entities_account_list)):
+    delegations_str = delegations_str + delegations_exp.replace('oasis1', entities_account_list[i]).replace('oasis2', entities_account_list[i]).replace('oasis3', nodes_account_list[i]) +','
+    #json_exp['staking']['delegations'][entities_account_list[i]][entities_account_list[i]]['shares'] = "4000000000000000"
+    #json_exp['staking']['delegations'][entities_account_list[i]][nodes_account_list[i]]['shares'] = 1000000000000000
+delegations_str = '['+delegations_str[:-1]+']'
+
+for i in json.loads(delegations_str):
+    json_exp['staking']['delegations'].update(i)
+
+
+# Add ledger to exp file
+leger_str = ''
+for i in range(len(entities_account_list)):
+    leger_str = leger_str + leger_example.replace('oasis1', entities_account_list[i]) + ','
+leger_str = '['+leger_str[:-1]+']'
+
+for i in json.loads(leger_str):
+    json_exp['staking']['ledger'].update(i)
+
+
+# Each ledger have 300000000000 token
+# staking.common_pool = staking.total_supply - len(ledger)*300000000000
+
+#print(json_gene_total)
+
+json_common_pool = json_gene_total - len(json.loads(leger_str))*300000000000
+
+json_exp['staking']['common_pool'] = str(json_common_pool)
+ 
+with open(example_path.replace('_example', '_result'), 'w') as outfile:
+    json.dump(json_exp, outfile)
---
+++ testnet/localnet/run.sh
@@ -0,0 +1,224 @@
+
+#!/bin/bash
+
+echo "  ___ ___ _      _      _   ___  "
+echo " |_ _/ __| |    | |    /_\ | _ ) "
+echo "  | |\__ \ |__  | |__ / _ \| _ \ "
+echo " |___|___/____| |____/_/ \_\___/ "
+echo
+echo
+
+echo
+echo "It will create 30 nodes."
+read -p "Press [Enter] to continue... or [Control + c] to stop..."
+
+WORKDIR=/localnet6/
+
+NODELIST=/nodelist.txt
+
+mkdir -p $WORKDIR
+
+#LOCAL_IP=$(hostname -I)
+#LOCAL_IP=${STATIC_IP% }
+LOCAL_IP=140.113.164.35
+
+run_create_nodes_env(){
+
+    touch $NODELIST
+
+    cd $WORKDIR
+    # Setup nodes config file
+    for i in {0000..0030}; do 
+        node_name="nodes/node$i"
+        mkdir -m700 -p $node_name; 
+
+        entity_name="entities/entity$i"
+        mkdir -m700 -p $entity_name;
+
+        oasis-node registry entity init --signer.backend file --signer.dir $WORKDIR/entities/entity$i/
+
+        ENTITY_ID=$(cat $WORKDIR/entities/entity$i/entity.json | jq '.id')
+        ENTITY_ID=${ENTITY_ID:1:-1}
+
+        echo "node${i}_pub=$ENTITY_ID" >> $NODELIST
+        echo "Generate entity $i"
+    done
+
+    echo ""
+    echo "Init node."
+
+    # Supported values are "compute-worker", "storage-worker", 
+    # "transaction-scheduler", "key-manager", "merge-worker", and "validator"
+
+    # Set up:
+    #compute-worker:5
+    #storage-worker:5
+    #transaction-scheduler:5
+    #merge-worker:5
+    #validator:10
+
+    # for i in {0000..0005}; do 
+    #     cd $WORKDIR/nodes/node$i
+
+    #     oasis-node registry node init \
+    #         --signer.backend file \
+    #         --signer.dir $WORKDIR/entities/entity$i \
+    #         --node.consensus_address $LOCAL_IP:26656 \
+    #         --node.is_self_signed \
+    #         --node.role compute-worker
+    # done
+
+    # for i in {0006..0010}; do 
+    #     cd $WORKDIR/nodes/node$i
+
+    #     oasis-node registry node init \
+    #         --signer.backend file \
+    #         --signer.dir $WORKDIR/entities/entity$i \
+    #         --node.consensus_address $LOCAL_IP:26656 \
+    #         --node.is_self_signed \
+    #         --node.role storage-worker
+    # done
+
+    # for i in {0011..0015}; do 
+    #     cd $WORKDIR/nodes/node$i
+
+    #     oasis-node registry node init \
+    #         --signer.backend file \
+    #         --signer.dir $WORKDIR/entities/entity$i \
+    #         --node.consensus_address $LOCAL_IP:26656 \
+    #         --node.is_self_signed \
+    #         --node.role transaction-scheduler
+    # done
+
+    # for i in {0016..0020}; do 
+    #     cd $WORKDIR/nodes/node$i
+
+    #     oasis-node registry node init \
+    #         --signer.backend file \
+    #         --signer.dir $WORKDIR/entities/entity$i \
+    #         --node.consensus_address $LOCAL_IP:26656 \
+    #         --node.is_self_signed \
+    #         --node.role merge-worker
+    # done
+
+    #for i in {0021..0030}; do
+    for i in {0000..0030}; do 
+        cd $WORKDIR/nodes/node$i
+
+        oasis-node registry node init \
+            --signer.backend file \
+            --signer.dir $WORKDIR/entities/entity$i \
+            --node.consensus_address $LOCAL_IP:26656 \
+            --node.is_self_signed \
+            --node.role validator
+    done
+
+    echo ""
+    echo "Update entity."
+
+    for i in {0000..0030}; do 
+        cd $WORKDIR/entities/entity$i
+
+        oasis-node registry entity update \
+            --signer.backend file \
+            --signer.dir $WORKDIR/entities/entity$i \
+            --entity.node.descriptor $WORKDIR/nodes/node$i/node_genesis.json
+    done
+
+    cd $WORKDIR 
+}
+
+run_setup_genesis_env(){
+    
+    mkdir -m700 -p $WORKDIR/genesis
+
+    cd $WORKDIR/genesis
+
+    # oasis-node genesis init \
+    #     --chain.id isltestnet \
+    #     --entity $WORKDIR/entities/entity0000/entity_genesis.json \
+    #     --node $WORKDIR/nodes/node0000/node_genesis.json \
+    #     --staking.token_symbol QAQ
+
+    oasis-node genesis init \
+        --chain.id isltestnet \
+        --entity $WORKDIR/entities/entity0000/entity_genesis.json \
+        --entity $WORKDIR/entities/entity0001/entity_genesis.json \
+        --entity $WORKDIR/entities/entity0002/entity_genesis.json \
+        --entity $WORKDIR/entities/entity0003/entity_genesis.json \
+        --entity $WORKDIR/entities/entity0004/entity_genesis.json \
+        --entity $WORKDIR/entities/entity0005/entity_genesis.json \
+        --entity $WORKDIR/entities/entity0006/entity_genesis.json \
+        --entity $WORKDIR/entities/entity0007/entity_genesis.json \
+        --entity $WORKDIR/entities/entity0008/entity_genesis.json \
+        --entity $WORKDIR/entities/entity0009/entity_genesis.json \
+        --entity $WORKDIR/entities/entity0010/entity_genesis.json \
+        --entity $WORKDIR/entities/entity0011/entity_genesis.json \
+        --entity $WORKDIR/entities/entity0012/entity_genesis.json \
+        --entity $WORKDIR/entities/entity0013/entity_genesis.json \
+        --entity $WORKDIR/entities/entity0014/entity_genesis.json \
+        --entity $WORKDIR/entities/entity0015/entity_genesis.json \
+        --entity $WORKDIR/entities/entity0016/entity_genesis.json \
+        --entity $WORKDIR/entities/entity0017/entity_genesis.json \
+        --entity $WORKDIR/entities/entity0018/entity_genesis.json \
+        --entity $WORKDIR/entities/entity0019/entity_genesis.json \
+        --node $WORKDIR/nodes/node0000/node_genesis.json \
+        --node $WORKDIR/nodes/node0001/node_genesis.json \
+        --node $WORKDIR/nodes/node0002/node_genesis.json \
+        --node $WORKDIR/nodes/node0003/node_genesis.json \
+        --node $WORKDIR/nodes/node0004/node_genesis.json \
+        --node $WORKDIR/nodes/node0005/node_genesis.json \
+        --node $WORKDIR/nodes/node0006/node_genesis.json \
+        --node $WORKDIR/nodes/node0007/node_genesis.json \
+        --node $WORKDIR/nodes/node0008/node_genesis.json \
+        --node $WORKDIR/nodes/node0009/node_genesis.json \
+        --node $WORKDIR/nodes/node0010/node_genesis.json \
+        --node $WORKDIR/nodes/node0011/node_genesis.json \
+        --node $WORKDIR/nodes/node0012/node_genesis.json \
+        --node $WORKDIR/nodes/node0013/node_genesis.json \
+        --node $WORKDIR/nodes/node0014/node_genesis.json \
+        --node $WORKDIR/nodes/node0015/node_genesis.json \
+        --node $WORKDIR/nodes/node0016/node_genesis.json \
+        --node $WORKDIR/nodes/node0017/node_genesis.json \
+        --node $WORKDIR/nodes/node0018/node_genesis.json \
+        --node $WORKDIR/nodes/node0019/node_genesis.json \
+        --staking.token_symbol QAQ
+
+    # rejq is a tool to reformat json file. (see /rejq.sh)
+    #rejq $WORKDIR/genesis/genesis.json
+
+    wget https://github.com/Intelligent-Systems-Lab/oasis-lab-dev/raw/master/testnet/genesis_preprocess/genesis_example.json
+
+    python3 /merge.py --genesis_path $WORKDIR/genesis/genesis.json  --example_path $WORKDIR/genesis/genesis_example.json
+
+    #bash /./rejq.sh $WORKDIR/genesis/genesis.json
+    rm genesis.json genesis_example.json
+    mv genesis_result.json genesis.json
+
+    #rm genesis_result.json
+
+    bash /./rejq.sh $WORKDIR/genesis/genesis.json
+
+    sed -i "s/18446744073709552000/18446744073709551615/" $WORKDIR/genesis/genesis.json
+
+    #wget https://gist.github.com/tony92151/3cd804235481c718d4c33b775dbf239b/raw/4905266215e66d63b33a31764891fedb1d0576d3/config.yml
+
+    #oasis-node --config ./config.yml
+    
+    # oasis-node \
+    #     --datadir $WORKDIR/nodes/node0000 \
+    #     --genesis.file $WORKDIR/genesis/genesis.json \
+    #     --worker.registration.entity $WORKDIR/entities/entity0000/entity.json \
+    #     --consensus.validator \
+    #     --debug.dont_blame_oasis \
+    #     --debug.allow_test_keys \
+    #     --log.level debug  
+
+    #>> genesis.log 2>&1 &
+}
+
+
+
+run_create_nodes_env
+
+run_setup_genesis_env
